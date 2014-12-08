b093df7b26c92d55a15efcac12e06a2654a6bf1e
cinnamoncoin <cinnamon.carter@aol.com>
2014-12-08 02:11:56 -0500

    update version number
    update version number

--- src/clientversion.h
+++ src/clientversion.h
@@ -9,7 +9,7 @@
 #define CLIENT_VERSION_MAJOR       2
 #define CLIENT_VERSION_MINOR       1
 #define CLIENT_VERSION_REVISION    0
-#define CLIENT_VERSION_BUILD       6 /*increase each release*/
+#define CLIENT_VERSION_BUILD       7 /*increase each release*/
 
 // Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE  true
